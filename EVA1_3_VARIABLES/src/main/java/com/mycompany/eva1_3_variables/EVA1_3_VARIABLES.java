001ef1b7d7e6d22e7ace9440794478c9830280de
JLOPVL33 <L24550321@chihuahua2.tecnm.mx>
2024-08-26 23:52:10 -0600

    Add files via upload

---
+++ EVA1_3_VARIABLES/src/main/java/com/mycompany/eva1_3_variables/EVA1_3_VARIABLES.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva1_3_variables;
+
+/**
+ *
+ * @author Jennifer
+ */
+public class EVA1_3_VARIABLES {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        //VEHICULOS
+        String marca;
+        String modelo;
+        int year;      //LA Ñ NO SE USA EN OTROS LENGUAJES ASI QUE DEBEMOS DE EMPEZAR A EVITARLA PARA NO ACOSTUMBRARNOS A USARLA
+        double precio;
+        
+        //ASIGNACIÓN
+        marca = "NISSAN";
+        modelo = "GTR";
+        year = 2008;
+        precio = 1000000.1;
+        
+        System.out.println(marca);
+        System.out.println(modelo);
+        System.out.println(year);
+        System.out.println(precio);
+        
+    }
+}
