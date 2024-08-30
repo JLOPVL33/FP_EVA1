0fce15aef2369cca798b383c0de809b0e4a09dcd
JLOPVL33 <L24550321@chihuahua2.tecnm.mx>
2024-08-29 23:10:34 -0600

    Add files via upload

---
+++ EVA1_4_TV/src/main/java/com/mycompany/eva1_4_tv/EVA1_4_TV.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva1_4_tv;
+
+/**
+ *
+ * @author Jennifer
+ */
+public class EVA1_4_TV {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        
+        //DECLARAR VARIABLES
+        String marca;
+        String modelo;
+        String tecnología;
+        int tamaño;
+        double precio;
+        
+        //ASIGNAR EL VALOR
+        marca = "Samsung";
+        modelo = "QN900D";
+        tecnología = "QLED";
+        tamaño = 85;
+        precio = 197990.0;
+        
+        //IMPRIMIR EL VALOR
+        System.out.println(marca);
+        System.out.println(modelo);
+        System.out.println(tecnología);
+        System.out.println(tamaño);
+        System.out.println(precio);
+        
+          
+        
+    }
+}
