0f2f27b41873899a9b26dd328fca76b483bf8fe1
JLOPVL33 <L24550321@chihuahua2.tecnm.mx>
2024-09-04 13:44:24 -0600

    Add files via upload

---
+++ EVA1_9_OPERACIONES/src/main/java/com/mycompany/eva1_9_operaciones/EVA1_9_OPERACIONES.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva1_9_operaciones;
+
+/**
+ *
+ * @author Jennifer
+ */
+public class EVA1_9_OPERACIONES {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        //OPERACIONES ARITMETICAS
+        //INICIALIZACIÓN DE UNA VARIABLE
+        int x = 10;
+        int y = 5;   
+        
+        int suma, resta, multiplicación, división, potencia, raíz;
+        
+        suma = x + y;
+        System.out.println("La suma de x + y :");
+        System.out.println(suma);
+        
+    }
+}
