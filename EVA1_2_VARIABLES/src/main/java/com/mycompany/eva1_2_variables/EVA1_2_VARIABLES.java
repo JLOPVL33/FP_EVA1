24fa14353eb5a2f5b7c39f88459f4b6b3e5aff03
JLOPVL33 <L24550321@chihuahua2.tecnm.mx>
2024-09-04 13:38:17 -0600

    Add files via upload

---
+++ EVA1_2_VARIABLES/src/main/java/com/mycompany/eva1_2_variables/EVA1_2_VARIABLES.java
@@ -0,0 +1,59 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva1_2_variables;
+
+/**
+ *
+ * @author Jennifer
+ */
+public class EVA1_2_VARIABLES {
+
+    public static void main(String[] args) {
+        System.out.println("HOLA MUNDO!!");
+        System.out.println(100);
+        System.out.println(3.1416);
+        System.out.println('c');
+        System.out.println(true);
+        System.out.println(false);
+        
+        //ENTEROS
+        //imt
+        //long
+        //short
+        
+        //NUMEROS CON PARTE DECIMAL (FLOTANTES)
+        //float
+        //double
+        
+        //CADENAS DE TEXTO
+        //String
+        
+        //CARACTERES
+        //char
+        
+        //VALORES LÓGICOS
+        //boolean
+        
+        //DECLARACIÓN DE VARIABLES
+        //SINTAXIS PARA DECLARAR VARIABLES (REGLAS)
+        //TIPO_DE_DATO NOMBRE_DE_LA_VARIABLE
+        String nombre;
+        String apellido;
+        String curp;
+        double promedio;
+        int edad;
+        
+        edad = 18; //ASIGNACIÓN, ESTOY COLOCANDO EL NUMERO 18DENTRO DE LA VARIABLE EDAD
+        
+        nombre = "JENNIFER LILIANA";
+        apellido = "LOPEZ ORTIZ";
+        promedio = 8.9;
+        
+        System.out.println(edad);
+        System.out.println(nombre);
+        System.out.println(apellido);
+        System.out.println(promedio);
+    }
+}
