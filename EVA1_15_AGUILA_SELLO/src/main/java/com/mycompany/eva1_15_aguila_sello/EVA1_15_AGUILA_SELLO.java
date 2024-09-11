d6b36a3dfe41c3a72afec50e289a1ea2ff5d5f24
JLOPVL33 <L24550321@chihuahua2.tecnm.mx>
2024-09-11 00:25:17 -0600

    Add files via upload

---
+++ EVA1_15_AGUILA_SELLO/src/main/java/com/mycompany/eva1_15_aguila_sello/EVA1_15_AGUILA_SELLO.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva1_15_aguila_sello;
+
+/**
+ *
+ * @author Jennifer
+ */
+public class EVA1_15_AGUILA_SELLO {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        //0.0---->0.99999999999999
+        //0.5
+        //0 1 2 3 4 5 6 7 8 9
+        double valor = Math.random();
+        if (valor < 0.5){
+           System.out.println("Aguila");
+    }else{
+           System.out.println("Sello");
+           
+        
+        }
+    }
+}
+
+           
+
